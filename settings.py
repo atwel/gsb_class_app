52f58f33c27ae1aef9ea1de48aa9ca32fa0697a1
Jonathan Atwell <jonathanatwell@GSB-C02YQ00DJV43.local>
2020-04-29 10:53:58 -0700

    resource fix

--- settings.py
+++ settings.py
@@ -49,7 +49,7 @@ ROOMS = [
 DEMO_PAGE_INTRO_HTML = """
 Here are some oTree games.
 """
-ADMIN_USERNAME = "Atwell"
+ADMIN_USERNAME = "atwell"
 ADMIN_PASSWORD = "aaaa"
 # don't share this with anybody.
 SECRET_KEY = 'z$k1!j!#nzwc#1af2%x9$^vde#u*(mf$wbdrmsw4f=$u2@jo!e'
