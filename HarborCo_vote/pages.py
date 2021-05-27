0576983e893e2c5f5a8481fbf202ae51154125bb
Jonathan Atwell <jonathanatwell@GSB-C02YQ00DJV43.local>
2021-05-27 09:19:25 -0700

    fix

--- HarborCo_vote/pages.py
+++ HarborCo_vote/pages.py
@@ -116,11 +116,11 @@ class Tally(WaitPage):
 
 
 
-        print(time.time(), self.group.in_round(1).start_time)
-        print("time passed", (time.time() - self.group.in_round(1).start_time)/60)
-        if (time.time() - self.group.in_round(1).start_time) / 60 > 75:
-            if not any([self.group.passed, self.group.high_passed]):
-                self.group.timed_out = True
+        #print(time.time(), self.group.in_round(1).start_time)
+        #print("time passed", (time.time() - self.group.in_round(1).start_time)/60)
+        #if (time.time() - self.group.in_round(1).start_time) / 60 > 75:
+        #    if not any([self.group.passed, self.group.high_passed]):
+        #        self.group.timed_out = True
 
 
 
