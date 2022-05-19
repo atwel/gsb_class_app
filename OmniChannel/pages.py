03f87d9eebaf8017fa0ed236beda6c86af52c75c
Jonathan Atwell <jonathanatwell@GSB-C02YQ00DJV43.local>
2022-05-19 15:25:44 -0700

    OmniChannel update

--- OmniChannel/pages.py
+++ OmniChannel/pages.py
@@ -7,9 +7,21 @@ from .models import Constants
 class Introduction(Page):
     form_model = "player"
 
+    def before_next_page(self):
+        try:
+            self.participant.vars["SUNet"] = self.participant.label
+            self.participant.vars["name"] = SUNet_to_name[self.participant.label]
+        except:
+            self.participant.vars["SUNet"] = "none"
+            self.participant.vars["name"] = "(come see Dr. Atwell)"
+
+        self.player.name = self.participant.vars["name"]
 
 class DTV(Page):
     form_model = "player"
+
+    timeout_seconds= Constants.reading_time * 60
+    timer_text = 'Time left for reading the materials'
 
     def is_displayed(self):
         return self.player.role() == "3dtv"
@@ -17,15 +29,11 @@ class DTV(Page):
     def vars_for_template(self):
         return {"pdf_file": "OmniChannel/3DTV.pdf"}
 
-class Message_DTV(Page):
-    form_model = "player"
-
-    def is_displayed(self):
-        return self.player.role() == "3dtv"
-
-
 class Omni(Page):
     form_model = "player"
+
+    timeout_seconds= Constants.reading_time * 60
+    timer_text = 'Time left for reading the materials'
 
     def is_displayed(self):
         return self.player.role() == "omni"
@@ -33,21 +41,24 @@ class Omni(Page):
     def vars_for_template(self):
         return {"pdf_file": "OmniChannel/OmniChannel.pdf"}
 
+class Message_DTV(Page):
+    form_model = "player"
+
+    def is_displayed(self):
+        return self.player.role() == "3dtv"
+
 class Message_OC(Page):
     form_model = "player"
 
     def is_displayed(self):
         return self.player.role() == "omni"
 
-
-class Agreement(Page):
-    form_model = "player"
-
-    form_fields = ["agreement"]
-
 class Planning_doc(Page):
     form_model = "player"
     form_fields = ["planning_text"]
+
+    timeout_seconds= Constants.planning_doc_time * 60
+    timer_text = 'Time left to finish writing your planning document'
 
     def vars_for_template(self):
         if self.player.role() == "3dtv":
@@ -60,13 +71,22 @@ class Wait_until_open(Page):
     #remove  next line when not demo-ing
     #timeout_seconds = 10
 
+class Agreement(Page):
+    form_model = "player"
+    form_fields = ["agreement"]
+
+    def is_displayed(self):
+        if self.player.role() =="3dtv":
+            return True
+        else:
+            return False
+
 class Outcome(Page):
     form_model = "player"
-
     form_fields = ["data","license_restrictions","premium_count","premium_fees","regular_count","regular_fees","data_center_fees","length","termination"]
 
     def is_displayed(self):
-        if self.player.agreement =="Yes":
+        if self.player.role() =="3dtv" and self.player.agreement =="Yes":
             return True
         else:
             return False
@@ -78,8 +98,6 @@ class Journaling_page(Page):
     form_model = "player"
 
     form_fields = ["journaling_text"]
-
-    #timeout_seconds = Constants.reflection_time*60
 
 class Outro(Page):
     form_model = "group"
