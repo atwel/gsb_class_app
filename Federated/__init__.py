eda22bb8fb844b08aa99a539ef871af8337244dd
Jonathan Atwell <jonathanatwell@GSB-C02YQ00DJV43.local>
2023-11-16 16:59:31 -0800

    FS ready

--- Federated/__init__.py
+++ Federated/__init__.py
@@ -15,13 +15,13 @@ class C(BaseConstants):
     NAME_IN_URL = 'Federated_Sciences'
     PLAYERS_PER_GROUP = 3
     NUM_ROUNDS = 1
-    READING_TIME = 10  # minutes
-    MATERIAL_BUTTON_SHOW = 2  # minutes
+    READING_TIME = 10
     PLANNING_DOC_TIME = 10
-    NEGOTIATING_TIME = 30  # minutes
-    REFLECTION_TIME = 5  # minutes
-    PLANNING_DOC_LENGTH = 100  # words
-
+    NEGOTIATING_TIME = 30
+    CLASSCODE = 180595
+    PLANNING_ASSIGNMENT_CODE = 542285
+    REFLECTION_ASSIGNMENT_CODE = 542286
+    FEEDBACK_ASSIGNMENT_CODE = 560154
 
 class Subsession(BaseSubsession):
     pass
@@ -36,7 +36,6 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     name = models.StringField()
-    # planning_text = models.LongStringField(label="Describe your plan for this negotiation. In particular, how do intend to approach dealing with coalitions?")
     united = models.IntegerField()
     turbo = models.IntegerField()
     stockman = models.IntegerField()
@@ -45,29 +44,16 @@ class Player(BasePlayer):
         choices=["Stockman", "United", "Both"],
         widget=widgets.RadioSelectHorizontal,
     )
-    # journaling_text = models.LongStringField(label="Please describe your experience of the negotiation.")
-
+    partner_name = models.StringField()
+    feedback = models.BooleanField()
+    consent = models.BooleanField(label="Are you willing to RECEIVE constructive feedback from your negotiation partner?")
+    review_consent = models.BooleanField(label="Similarly, are you willing to GIVE constructive feedback to your negotiation partner?")
 
 # FUNCTIONS
 def creating_session(subsession: Subsession):
     stock = itertools.cycle([True, False])
     for g in subsession.get_groups():
         g.stockman = next(stock)
-
-
-def vars_for_admin_report(subsession: Subsession):
-    stockman = []
-    united = []
-    turbo = []
-    for p in subsession.get_players():
-        the_label = p.participant.vars["name"]
-        if p.role() == "stockman":
-            stockman.append(the_label)
-        elif p.role() == "turbo":
-            turbo.append(the_label)
-        else:
-            united.append(the_label)
-    return {"Stockman": stockman, "Turbo": turbo, "United": united}
 
 
 def set_end_time(group: Group):
@@ -107,17 +93,73 @@ def role(player: Player):
 
 # PAGES
 SUNet_to_name = {
-    "extra1": "Unnamed #1",
-    "extra2": "Unnamed #2",
-    "extra3": "Unnamed #3",
-    "extra4": "Unnamed #4",
-    "extra5": "Unnamed #5",
-    "extra6": "Unnamed #6",
-    "extra7": "Unnamed #7",
-    "extra8": "Unnamed #8",
-    "extra9": "Unnamed #9",
-    "extra10": "Unnamed #10",
-}
+    "Extra_1": "Unnamed #1",
+    "Extra_2": "Unnamed #2",
+    "Extra_3": "Unnamed #3",
+    "Extra_4": "Unnamed #4",
+    "Extra_5": "Unnamed #5",
+    "Extra_6": "Unnamed #6",
+    "Extra_7": "Unnamed #7",
+    "Extra_8": "Unnamed #8",
+    "Extra_9": "Unnamed #9",
+    "Extra_10": "Unnamed #10",
+    "addachan":"Addie Achan",
+    "sbeaton":"Stephen Beaton",
+    "lbrito":"Louise Brito",
+    "vcharoon":"Victor Charoonsophonsak",
+    "rchun1":"Robert Chun",
+    "dowlingp":"Patrick Dowling",
+    "laurajg":"Laura Griffiths",
+    "ruthguan":"Ruth Guan",
+    "whooper":"Whitney Hooper",
+    "exyhuang":"Eileen Huang",
+    "thuang24":"Tony Huang",
+    "adrianus":"Adrian Hunggara",
+    "shubhij":"Shubhi Jain",
+    "noorissa":"Noorissa Khoja",
+    "lroberds":"Lia Lilleness",
+    "clifflim":"Cliff Lim",
+    "amaderoo":"Andrea Madero",
+    "mcgarryg":"Gavin McGarry",
+    "knyman":"Knut Nyman",
+    "jphaneuf":"Jeff Phaneuf",
+    "dansegev":"Dan Segev",
+    "bsinghla":"Bharti Singhla",
+    "smithc52":"Christian Smith",
+    "sesuarez":"Sofia Suarez",
+    "avaldi":"Adolfo Valdivieso Quiroz",
+    "gbreeves":"Bear Vasquez",
+    "bwilber":"Bryce Wilberding",
+    "ijdelcid":"Imer del Cid",
+    "nnandrew":"Nick Andrews",
+    "ebendezu":"Edgar Bendezú",
+    "darapc":"Dara Canavan",
+    "nchedid":"Nicholas Chedid",
+    "tdodson":"Trey Dodson III",
+    "storeydk":"Storey Dyer Kloman",
+    "aevenson":"Austin Evenson",
+    "onf":"Oren Fliegelman",
+    "asjfu":"Allison Fu",
+    "lfunke":"Lennart Funke",
+    "dhersh":"Daniel Hersh",
+    "cjanis":"Chad Janis",
+    "sjonn":"Sarah Jonn",
+    "ajow":"Alex Jow",
+    "alacey":"Alex Lacey",
+    "rlhannah":"Hannah Lee",
+    "brlobato":"Breno Lobato",
+    "grantmcn":"Grant McNaughton",
+    "gaamello":"Gui Mello",
+    "hneffa":"Henrique Neffa",
+    "rspark":"Rachel Park",
+    "pressler":"Sam Pressler",
+    "athomp10":"Alexander Thompson",
+    "jamesu":"James Underwood",
+    "jaw33":"Jelani Williamson",
+    "paulyap":"Paul Yap",
+    "alexyin":"Alex Yin",
+    "sszou ":"Sophia Sun Alex",
+    "jdacosta":"James da Costa"}
 
 
 class IntroWaitPage(WaitPage):
@@ -137,141 +179,116 @@ class Introduction(Page):
     @staticmethod
     def before_next_page(player: Player, timeout_happened):
         try:
-            player.participant.vars["SUNet"] = player.participant.label
-            player.participant.vars["name"] = SUNet_to_name[player.participant.label]
+            player.name = SUNet_to_name[player.participant.label]
         except:
-            player.participant.vars["SUNet"] = "none"
-            player.participant.vars["name"] = "(come see Dr. Atwell)"
-        player.name = player.participant.vars["name"]
+            player.name = "(come see Dr. Atwell)"
 
 
 class Stockman(Page):
     form_model = "player"
-    # timeout_seconds = C.READING_TIME*60
-    # timer_text = 'Time left for reading the materials'
+    timeout_seconds = C.READING_TIME*60
+    timer_text = 'Time left for reading the materials'
+
     @staticmethod
     def is_displayed(player: Player):
         return role(player) == "stockman"
 
     @staticmethod
     def vars_for_template(player: Player):
-        return {"pdf_file": "Federated/Stockman.pdf", "End_time": "", "show_end_time": False}
-
-    @staticmethod
-    def js_vars(player: Player):
-        return dict(button_show=C.MATERIAL_BUTTON_SHOW * 60000)
+        return {"pdf_file": "Federated/Stockman.pdf"}
 
 
 class Stockman_nt(Page):
     form_model = "player"
     template_name = "Federated/Stockman.html"
 
+    timeout_seconds = C.NEGOTIATING_TIME*60
+    timer_text = 'Time left for negotiating'
     @staticmethod
     def is_displayed(player: Player):
         return role(player) == "stockman"
 
     @staticmethod
     def vars_for_template(player: Player):
-        return {
-            "pdf_file": "Federated/Stockman.pdf",
-            "End_time": player.group.end_time,
-            "show_end_time": False,
-        }
-
-    @staticmethod
-    def js_vars(player: Player):
-        return dict(button_show=C.MATERIAL_BUTTON_SHOW * 60000)
+        return {"pdf_file": "Federated/Stockman.pdf"}
 
 
 class Turbo(Page):
     form_model = "player"
-    # timeout_seconds = C.READING_TIME*60
-    # timer_text = 'Time left for reading the materials'
+    timeout_seconds = C.READING_TIME*60
+    timer_text = 'Time left for reading the materials'
+
     @staticmethod
     def is_displayed(player: Player):
         return role(player) == "turbo"
 
     @staticmethod
     def vars_for_template(player: Player):
-        return {"pdf_file": "Federated/Turbo.pdf", "End_time": "", "show_end_time": False}
-
-    @staticmethod
-    def js_vars(player: Player):
-        return dict(button_show=C.MATERIAL_BUTTON_SHOW * 60000)
+        return {"pdf_file": "Federated/Turbo.pdf"}
 
 
 class Turbo_nt(Page):
     form_model = "player"
     template_name = "Federated/Turbo.html"
 
+    timeout_seconds = C.NEGOTIATING_TIME*60
+    timer_text = 'Time left for negotiating'
+
     @staticmethod
     def is_displayed(player: Player):
         return role(player) == "turbo"
 
     @staticmethod
     def vars_for_template(player: Player):
-        return {
-            "pdf_file": "Federated/Turbo.pdf",
-            "End_time": player.group.end_time,
-            "show_end_time": False,
-        }
-
-    @staticmethod
-    def js_vars(player: Player):
-        return dict(button_show=C.MATERIAL_BUTTON_SHOW * 60000)
+        return {"pdf_file": "Federated/Turbo.pdf"}
 
 
 class United(Page):
     form_model = "player"
-    # timeout_seconds = C.READING_TIME*60
-    # timer_text = 'Time left for reading the materials'
+    timeout_seconds = C.READING_TIME*60
+    timer_text = 'Time left for reading the materials'
+
     @staticmethod
     def is_displayed(player: Player):
         return role(player) == "united"
 
     @staticmethod
     def vars_for_template(player: Player):
-        return {"pdf_file": "Federated/United.pdf", "End_time": "", "show_end_time": False}
+        return {"pdf_file": "Federated/United.pdf"}
 
-    @staticmethod
-    def js_vars(player: Player):
-        return dict(button_show=C.MATERIAL_BUTTON_SHOW * 60000)
 
 
 class United_nt(Page):
     form_model = "player"
     template_name = "Federated/United.html"
 
+    timeout_seconds = C.NEGOTIATING_TIME*60
+    timer_text = 'Time left for negotiating'
+
     @staticmethod
     def is_displayed(player: Player):
         return role(player) == "united"
 
     @staticmethod
     def vars_for_template(player: Player):
-        return {
-            "pdf_file": "Federated/United.pdf",
-            "End_time": player.group.end_time,
-            "show_end_time": False,
-        }
+        return {"pdf_file": "Federated/United.pdf"}
 
-    @staticmethod
-    def js_vars(player: Player):
-        return dict(button_show=C.MATERIAL_BUTTON_SHOW * 60000)
 
 
 class Planning_doc(Page):
     form_model = "player"
-    # form_fields = ["planning_text"]
-    # timeout_seconds = C.PLANNING_DOC_TIME *60
-    # timer_text = "Time left to finish the planning document"
+    timeout_seconds = C.PLANNING_DOC_TIME *60
+    timer_text = "Time left to finish the planning document"
+
     @staticmethod
     def vars_for_template(player: Player):
+        url  = "/{}/assignments/{}".format(C.CLASSCODE, C.PLANNING_ASSIGNMENT_CODE)
         if role(player) == "stockman":
-            return {"pdf_file": "Federated/Stockman.pdf", "assignment_url":"/173725/assignments/514461"}
+            return {"pdf_file": "Federated/Stockman.pdf", "assignment_url":url}
         elif role(player) == "turbo":
-            return {"pdf_file": "Federated/Turbo.pdf", "assignment_url":"/173725/assignments/514461"}
+            return {"pdf_file": "Federated/Turbo.pdf", "assignment_url":url}
         elif role(player) == "united":
-            return {"pdf_file": "Federated/United.pdf", "assignment_url":"/173725/assignments/514461"}
+            return {"pdf_file": "Federated/United.pdf", "assignment_url":url}
 
 
 class Ready_for_class(Page):
@@ -359,11 +376,45 @@ class Outcome(Page):
     form_fields = ["united", "stockman", "turbo", "first_meeting"]
 
 
-class Journaling_page(Page):
+class Feedback_consent(Page):
     form_model = "player"
-    def vars_for_template(player: Player):
-        return {"assignment_url": "/173725/assignments/514463"}
+    form_fields = ["consent", "review_consent"]
 
+class ConsentWaitPage(WaitPage):
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        return {
+            "title_text": "Waiting...",
+            "body_text": "Please wait while your partner considers whether they want feedback",
+        }
+
+class Reflection_page(Page):
+    form_model = "player"
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        feedback_url = "/{}/assignments/{}".format(C.CLASSCODE, C.FEEDBACK_ASSIGNMENT_CODE)
+        reflection_url = "/{}/assignments/{}".format(C.CLASSCODE, C.REFLECTION_ASSIGNMENT_CODE)
+
+        if player.review_consent:
+            willing = []
+            for p in player.get_others_in_group():
+                if p.consent:
+                    willing.append(p.name)
+            if len(willing)==2:
+                fb_str="{} and {} are".format(willing[0], willing[1])
+                player.feedback = True
+            elif len(willing)==1:
+                fb_str = "{} is".format(willing[0])
+                player.feedback = True
+            else:
+                fb_str = ""
+                player.feedback = False
+        else:
+            player.feedback = False
+            fb_str=""
+        return {"feedback_url":feedback_url, "reflection_url":reflection_url, "feedback_names":fb_str}
 
 class Outro(Page):
     form_model = "group"
@@ -382,6 +433,8 @@ page_sequence = [
     Turbo_nt,
     United_nt,
     Outcome,
-    Journaling_page,
+    Feedback_consent,
+    ConsentWaitPage,
+    Reflection_page,
     Outro,
 ]
